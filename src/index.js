b008afb1b36f9e3211fbb2af231e04b8d43e25fb
Jelika <smirnova.lika.23092001@gmail.com>
2020-03-01 23:24:29 +0300

    add: index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix = []) {
+    return matrix.map((element, index)=> (index-1)%2? element : element.reverse()).flat();
 }
